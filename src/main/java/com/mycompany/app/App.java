2e1be488b9128f320ad1fbde1eb73787a2cbd288
himanshu5607 <157357053+himanshu5607@users.noreply.github.com>
2025-11-03 11:51:29 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,11 @@
-package com.example;
+package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
+    }
+
+    public String getMessage() {
+        return "Hello World!";
     }
 }
