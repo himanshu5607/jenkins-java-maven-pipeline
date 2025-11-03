4342d5a280a94f9ac666f43a5943f9df73de51eb
himanshu5607 <157357053+himanshu5607@users.noreply.github.com>
2025-11-03 11:51:58 +0530

    Refactor AppTest class and update test methods

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,24 +1,12 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest
-{
-    @Test
-    public void testAppConstructor() {
-        App app1 = new App();
-        App app2 = new App();
-        assertEquals(app1.getMessage(), app2.getMessage());
-    }
+public class AppTest {
 
     @Test
-    public void testAppMessage()
-    {
+    public void testAppHasMessage() {
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
     }
